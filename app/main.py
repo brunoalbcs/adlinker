e0dbe92fca7130f6f16e967b565f24b70b30e3ef
Bruno Alberto <brunoalbcs@gmail.com>
2026-08-28 07:41:39 -0300

    Atualização do título da API

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ import app.models  # É obrigatório importar os models aqui para o SQLAlchemy e
 Base.metadata.create_all(bind=engine)
 
 # 2. INICIALIZAÇÃO DA API
-app = FastAPI(title="AdLinker API - Arquitetura em Camadas")
+app = FastAPI(title="AdLinker API - Encurtador de Links")
 
 # 3. PLUGANDO AS ROTAS (O Roteador)
 app.include_router(link_router.router)
