e9bca096ba0f143d36ba0e56b1e3ace34d001e6f
Bruno Alberto <brunoalbcs@gmail.com>
2026-08-28 07:28:20 -0300

    fix: base url obtida automaticamente do request

--- app/routers/link_router.py
+++ app/routers/link_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, Request
 from fastapi.responses import RedirectResponse
 from app.schemas import LinkResponse, LinkRequest
 from sqlalchemy.orm import Session
@@ -10,8 +10,9 @@ from app.database import get_db
 router = APIRouter()
 
 @router.post("/", response_model=LinkResponse)
-def POST(link: LinkRequest, db: Session = Depends(get_db)):
-    return ls.criar_link_encurtado(db, str(link.link), "http://127.0.0.1:8000/")
+def POST(link: LinkRequest, request: Request, db: Session = Depends(get_db)):
+    base_url = str(request.base_url)
+    return ls.criar_link_encurtado(db, str(link.link), base_url)
 
 @router.get("/{codigo}")
 def REDIRECT(codigo: str, db: Session = Depends(get_db)):
